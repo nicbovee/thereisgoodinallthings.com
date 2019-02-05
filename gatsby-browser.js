d2c6b516222b58f455ad5e48cff5d1c27e31f6ac
Nicolas Bovee <admin@ghettifish.com>
2019-02-05 12:49:28 -0700

    Added plugin styled

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -9,7 +9,8 @@ import React from "react";
 import styled from "styled-components";
 import {ThemeContext, themes} from './src/theme-context';
 import {Helmet} from "react-helmet";
-
+import fav32 from "./src/favicons/favicon-32x32.png";
+import fav16 from "./src/favicons/favicon-16x16.png";
 class RootProvider extends React.Component {
   toggleTheme = () => {
     this.setState(state => ({
@@ -25,8 +26,8 @@ class RootProvider extends React.Component {
       <ThemeContext.Provider value={this.state}>
         <Helmet>
           <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href={fav32} />
+          <link rel="icon" type="image/png" sizes="16x16" href={fav16}/>
           <link rel="manifest" href="/favicons/site.webmanifest" />
           <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#ffffff" />
           <meta name="msapplication-TileColor" content="#ffffff" />
